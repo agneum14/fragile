e24bd0ec662b1b26d130bea81173867489aaab31
Joshua Hairston <hairs3ja@dukes.jmu.edu>
2023-11-01 13:16:44 -0400

    Display change

--- src/gui/Display.java
+++ src/gui/Display.java
@@ -1,42 +1,16 @@
 package gui;
 
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JTextArea;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JPanel;
 
-public class Display extends JTextArea implements ActionListener
+public class Display extends JPanel
 {
   private static final long serialVersionUID = 1L;
 
   public Display()
   {
-    super(4, 4);
-    setupLayout();
+    
   }
 
-  private void setupLayout()
-  {
-    setBorder(new EmptyBorder(5, 5, 5, 5));
-  }
-
-  public void reset()
-  {
-    this.setText("");
-  }
-
-  public Dimension getMaximumSize()
-  {
-    return getPreferredSize();
-  }
-
-  @Override
-  public void actionPerformed(ActionEvent e)
-  {
-    // TODO Auto-generated method stub
-
-  }
 
 }
