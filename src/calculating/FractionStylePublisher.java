372a863fe59f8b976a387f75176c0b968ee17112
neuma2ag <neuma2ag@dukes.jmu.edu>
2023-11-10 00:28:14 -0500

    add the FractionStylePublisher class and the FractionStyleSubscriber interface to facilitate changing the mixed fraction style on the fly

---
+++ src/calculating/FractionStylePublisher.java
@@ -0,0 +1,86 @@
+package calculating;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class notifies the FractionStyleSubscribers the mixed fraction style has changed. The
+ * currently supported styles are bar, solidus, and slash, all of which are detailed in the Domain
+ * Glossary.
+ *
+ * This code complies with the JMU Honor Code
+ *
+ * @author Andrew G. Neumann
+ * @version 1.0
+ */
+public class FractionStylePublisher
+{
+
+  /**
+   * The mixed fraction styles.
+   */
+  public enum FractionStyle
+  {
+    BAR, SOLIDUS, SLASH
+  }
+
+  private final List<FractionStyleSubscriber> subscribers;
+  private FractionStyle style;
+
+  /**
+   * This constructor initializes subscribers to an empty ArrayList and sets style to BAR.
+   */
+  public FractionStylePublisher()
+  {
+    subscribers = new ArrayList<>();
+    style = FractionStyle.BAR;
+  }
+
+  /**
+   * Add a FractionStyleSubscriber to the subscribers.
+   *
+   * @param subscriber
+   *     The FractionStyleSubscriber to add
+   */
+  public void addSubscriber(final FractionStyleSubscriber subscriber)
+  {
+    subscribers.add(subscriber);
+  }
+
+  /**
+   * Remove a FractionStyleSubscriber from the subscribers.
+   *
+   * @param subscriber
+   *     The FractionStyleSubscriber to remove
+   */
+  public void removeSubscriber(final FractionStyleSubscriber subscriber)
+  {
+    subscribers.remove(subscriber);
+  }
+
+  /**
+   * Notify all the subscribers the mixed fraction style has changed.
+   *
+   * @param fractionStyle
+   *     The updated mixed fraction style
+   */
+  public void notifyStyle(final FractionStyle fractionStyle)
+  {
+    this.style = fractionStyle;
+
+    for (FractionStyleSubscriber subscriber : subscribers)
+    {
+      subscriber.handleStyle(fractionStyle);
+    }
+  }
+
+  /**
+   * Getter for style.
+   *
+   * @return style
+   */
+  public FractionStyle getStyle()
+  {
+    return style;
+  }
+}
---
+++ src/calculating/FractionStyleSubscriber.java
@@ -0,0 +1,24 @@
+package calculating;
+
+import calculating.FractionStylePublisher.FractionStyle;
+
+/**
+ * This interface listens for changes in the mixed fraction style from the FractionStylePublisher.
+ * MixedFractionPanels implement this interface to update their display on the fly.
+ *
+ * This code complies with the JMU Honor Code
+ *
+ * @author Andrew G. Neumann
+ * @version 1.0
+ */
+public interface FractionStyleSubscriber
+{
+
+  /**
+   * Handle a new mixed fraction style.
+   *
+   * @param style
+   *     The new mixed fraction style
+   */
+  public void handleStyle(final FractionStyle style);
+}
