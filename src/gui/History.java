8d4bb2f61e914177bb80496c7b0afa053780703d
zbuchan <140669954+zbuchan@users.noreply.github.com>
2023-11-15 15:18:10 -0500

    Adding animation to the button pressed in History class opening and closing.

--- src/gui/History.java
+++ src/gui/History.java
@@ -3,85 +3,99 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JWindow;
+import javax.swing.Timer;
 
 /**
  * Class for the session history of the calculator
  * 
- * @author Joshua Hairston
+ * @author Joshua Hairston, Zachary Buchan
+ * 
  * 
  * @version 11/13/2023
  * 
  *          This Code Complies with the JMU Honor code.
  */
-public class History extends JWindow implements ActionListener
-{
-  private static final long serialVersionUID = 1L;
-  private static final int CLOSED = 0;
-  private static final int OPENED = 1;
-  private int status;
-  private JButton button;
+public class History extends JWindow implements ActionListener {
+    private static final long serialVersionUID = 1L;
+    private static final int CLOSED = 0;
+    private static final int OPENED = 1;
+    private int status;
+    private JButton button;
+    private Timer timer;
 
-  /**
-   * Constructor for the History class
-   */
-  public History(JFrame frame)
-  {
-    super(frame);
-    this.status = CLOSED; // Tracking the status of the window.
-    setupLayout();
-    setSize(30, 300);
-    setVisible(true);
-  }
-  /**
-   * Setting up the background and the layout of the history window.
-   */
-  private void setupLayout()
-  {
-    getContentPane().setBackground(Display.POWDER_BLUE);
-    setLayout(new BorderLayout());
-    button = new JButton(">");
-    button.addActionListener(this);
-    add(button, BorderLayout.EAST);
-
-  }
-  /**
-   * Method for checking to see if the window is either closed or open. 
-   */
-  @Override
-  public void actionPerformed(ActionEvent e)
-  {
-    String ac = e.getActionCommand();
-    if (ac.equals(">"))
-    {
-      if (status == CLOSED)
-      {
-        setSize(300, 300);
-        status = OPENED;
-        button.setText("<");
-
-      }
-
+    /**
+     * Constructor for the History class
+     */
+    public History(JFrame frame) {
+        super(frame);
+        this.status = CLOSED; // Tracking the status of the window.
+        setupLayout();
+        setSize(30, 300);
+        setVisible(true);
     }
-    else
-    {
-      setSize(30, 300);
-      status = CLOSED;
-      button.setText(">");
+
+    /**
+     * Setting up the background, the layout of the history window, and the animation.
+     */
+    private void setupLayout() {
+        getContentPane().setBackground(Display.POWDER_BLUE);
+        setLayout(new BorderLayout());
+        button = new JButton(">");
+        button.addActionListener(this);
+        add(button, BorderLayout.EAST);
+
+        
+        // animation
+        timer = new Timer(10, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                animate();
+            }
+        });
     }
-  }
 
-  public void setHistoryLocation(int x, int y)
-  {
-    this.setLocation(x, y);
+    /**
+     * Method for checking to see if the window is either closed or open. 
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String ac = e.getActionCommand();
+        if (ac.equals(">")) {
+            if (status == CLOSED) {
+                status = OPENED;
+                button.setText("<");
+                timer.start();
+            }
+        } else {
+            status = CLOSED;
+            button.setText(">");
+            timer.start();
+        }
+    }
 
-  }
-  
-  public void draw() {
-    
-  }
+    /**
+     * Animates the button.
+     */
+    private void animate() {
+        int targetWidth = (status == OPENED) ? 300 : 30;
+        int currentWidth = getWidth();
+
+        if (currentWidth != targetWidth) {
+            int newWidth = (currentWidth < targetWidth) ? currentWidth + 5 : currentWidth - 5;
+            setSize(newWidth, 300);
+
+            if (currentWidth < targetWidth && newWidth >= targetWidth) {
+                timer.stop();
+            } else if (currentWidth > targetWidth && newWidth <= targetWidth) {
+                timer.stop();
+            }
+        }
+    }
+
+    public void setHistoryLocation(int x, int y) {
+        setLocation(x, y);
+    }
 }
