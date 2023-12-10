646a07195a80a159804a97453d88f3a2776ef474
Stingray <44787842+OutboundSpade@users.noreply.github.com>
2023-12-10 17:25:49 -0500

    Update GuiConfig.java
    Add default config save

--- src/gui/GuiConfig.java
+++ src/gui/GuiConfig.java
@@ -1,34 +1,35 @@
 package gui;
 
-import java.awt.Color;
+import utilities.MapFormatter;
+
+import java.awt.*;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
-import utilities.MapFormatter;
 
 /**
  * Config Class for Fragile.
  */
 public class GuiConfig
 {
-  
+
   private Map<String, String> map;
-  
+
   /**
    * Constructor for the GUI's config.
    */
-  public GuiConfig() 
+  public GuiConfig()
   {
-    try 
+    try
     {
       map = MapFormatter.read("/html/config.txt");
-    }catch(FileNotFoundException e) 
+    } catch (FileNotFoundException e)
     {
       loadDefault();
     }
   }
-  
+
   /**
    * Loads default fragile if the config isn't present.
    */
@@ -39,28 +40,35 @@ public class GuiConfig
     map.put("red", "210");
     map.put("green", "237");
     map.put("blue", "255");
+    try
+    {
+      MapFormatter.write(map, "/html/config.txt");
+    } catch (IOException e)
+    {
+      throw new RuntimeException(e);
+    }
   }
-  
+
   /**
    * Getter for the color in the config.
-   * 
+   *
    * @return The color based on the stored RGB values in map
    */
-  public Color getColor() 
+  public Color getColor()
   {
     Integer red = Integer.parseInt(map.get("red"));
     Integer green = Integer.parseInt(map.get("green"));
     Integer blue = Integer.parseInt("blue");
     return new Color(red, green, blue);
   }
-  
+
   /**
    * Getter for the Logo path.
-   * 
+   *
    * @return The path string
    */
-  public String getLogoPath() 
+  public String getLogoPath()
   {
-    return map.get("logo"); 
+    return map.get("logo");
   }
 }
