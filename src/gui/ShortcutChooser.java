074dda5d6f1265a1ece1d93a09d2abe0ed67f4d1
Joshua Hairston <hairs3ja@dukes.jmu.edu>
2023-12-10 16:48:53 -0500

    updated keyboardshortcuts

--- src/gui/ShortcutChooser.java
+++ src/gui/ShortcutChooser.java
@@ -3,22 +3,18 @@ package gui;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenuItem;
-import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 
-import calculating.FractionModePublisher;
-import calculating.FractionStylePublisher;
-import calculating.FractionStylePublisher.FractionStyle;
+import utilities.MapFormatter;
 
 public class ShortcutChooser extends JFrame implements ActionListener
 {
@@ -26,9 +22,12 @@ public class ShortcutChooser extends JFrame implements ActionListener
   private JLabel label;
   private TextFieldHint text;
   private JButton set;
+  private JButton save;
+  private Map<String, String> keybinds;
 
   public ShortcutChooser()
   {
+    keybinds = new HashMap<>();
     makeGUI();
     this.setTitle("Shortcut Helper");
     this.setSize(300, 300);
@@ -47,25 +46,79 @@ public class ShortcutChooser extends JFrame implements ActionListener
     String prompt = "Please enter in a key for your keyboard shortcut\n\n(note the key will be paired with the Control key)";
     text = new TextFieldHint(prompt);
     set = new JButton("Set");
+    save = new JButton("Save");
     set.addActionListener(this);
+    save.addActionListener(this);
+    comboBox.addActionListener(this);
     setLayout(new FlowLayout());
     this.add(comboBox);
     this.add(text);
+    this.add(save);
     this.add(set);
 
+  }
+
+  public static void setKeybind(String menuItem, String character)
+  {
+    char c = character.charAt(0);
+    switch (menuItem)
+    {
+      case "Exit" -> Menu.exitMenuItem
+          .setAccelerator(KeyStroke.getKeyStroke(c, KeyEvent.CTRL_DOWN_MASK));
+      case "New Calculator" -> Menu.newCalcMenuItem
+          .setAccelerator(KeyStroke.getKeyStroke(c, KeyEvent.CTRL_DOWN_MASK));
+      case "Pie Chart" -> Menu.pieChartMenuItem
+          .setAccelerator(KeyStroke.getKeyStroke(c, KeyEvent.CTRL_DOWN_MASK));
+      case "Print Session" -> Menu.printMenuItem
+          .setAccelerator(KeyStroke.getKeyStroke(c, KeyEvent.CTRL_DOWN_MASK));
+      case "About" -> Menu.aboutMenuItem
+          .setAccelerator(KeyStroke.getKeyStroke(c, KeyEvent.CTRL_DOWN_MASK));
+      case "Shortcuts" -> Menu.shortcutsMenuItem
+          .setAccelerator(KeyStroke.getKeyStroke(c, KeyEvent.CTRL_DOWN_MASK));
+      case "Help" -> Menu.helpMenuItem
+          .setAccelerator(KeyStroke.getKeyStroke(c, KeyEvent.CTRL_DOWN_MASK));
+      case "Bar" -> Menu.barMenuItem
+          .setAccelerator(KeyStroke.getKeyStroke(c, KeyEvent.CTRL_DOWN_MASK));
+      case "Slash" -> Menu.slashMenuItem
+          .setAccelerator(KeyStroke.getKeyStroke(c, KeyEvent.CTRL_DOWN_MASK));
+      case "Solidus" -> Menu.solidusMenuItem
+          .setAccelerator(KeyStroke.getKeyStroke(c, KeyEvent.CTRL_DOWN_MASK));
+      case "Proper" -> Menu.properMenuItem
+          .setAccelerator(KeyStroke.getKeyStroke(c, KeyEvent.CTRL_DOWN_MASK));
+      case "Reduced" -> Menu.reducedMenuItem
+          .setAccelerator(KeyStroke.getKeyStroke(c, KeyEvent.CTRL_DOWN_MASK));
+      default -> System.out.println("unknown menu option");
+    }
   }
 
   @Override
   public void actionPerformed(ActionEvent e)
   {
-    if(e.getActionCommand().equals("Set")) {
-     String menuItemText = comboBox.getSelectedItem().toString();
-     KeyStroke shortcutKey = KeyStroke.getKeyStroke(text.getText().charAt(0), 0);
-     System.out.println(shortcutKey);
-     switch(menuItemText) {
-       case "Exit" -> Menu.exitMenuItem.setAccelerator(KeyStroke.getKeyStroke(shortcutKey.getKeyCode(), KeyEvent.CTRL_DOWN_MASK));
-       default -> System.out.println("unknown menu option");
-     }
+    if (e.getActionCommand().equals("Set"))
+    {
+      System.out.println(comboBox.getSelectedItem());
+      String menuItemText = comboBox.getSelectedItem().toString();
+      String character = text.getText().toUpperCase();
+      setKeybind(menuItemText, character);
+      keybinds.put(menuItemText,character);
+    }
+    else if (e.getSource().equals(comboBox))
+    {
+      comboBox.setSelectedItem(comboBox.getSelectedItem());
+      System.out.println(comboBox.getSelectedItem());
+    }
+    else if (e.getActionCommand().equals("Save"))
+    {
+      try
+      {
+        MapFormatter.write(keybinds, "shortcuts.txt");
+
+      }
+      catch (IOException e1)
+      {
+        // TODO Auto-generated catch block
+        e1.printStackTrace();
+      }
     }
 
   }
