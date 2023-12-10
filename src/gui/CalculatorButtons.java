b6db0452e63bb5da7da346a2cd4f04f726052786
neuma2ag <neuma2ag@dukes.jmu.edu>
2023-12-09 23:21:43 -0500

    remove send button and rework Display behavior to compensate

--- src/gui/CalculatorButtons.java
+++ src/gui/CalculatorButtons.java
@@ -37,8 +37,6 @@ public class CalculatorButtons extends JPanel implements ActionListener
   public static final String EQUAL_TO = "\u225D";
   public static final String OPEN_PAREN = "(";
   public static final String CLOSE_PAREN = ")";
-  public static final String SEND = "\u2191";
-  
 
   private static final long serialVersionUID = 1L;
 
@@ -220,12 +218,9 @@ public class CalculatorButtons extends JPanel implements ActionListener
     ActionMap actionMap = this.getActionMap();
     actionMap.put(POSITION, new PressAction(button));
     add(button, c);
-   
-    c.gridx = 3;
-    add(setButton(SEND, AQUA), c);
 
     // The equals button
-    c.gridx = 4;
+    c.gridx = 3;
     add(setButton(EQUALS, AQUA), c);
 
     c.gridx = 5;
